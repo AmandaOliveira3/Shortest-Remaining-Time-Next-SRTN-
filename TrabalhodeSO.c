57870e46a6d9da63b412d2701a81966555dbf2f1
Amanda Oliveira <106553498+AmandaOliveira3@users.noreply.github.com>
2023-10-26 23:47:41 -0300

    Add files via upload

--- TrabalhodeSO.c
+++ TrabalhodeSO.c
@@ -1,72 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <time.h>
 
 #define NUM_PROCESSOS 10
 
-typedef enum eStatus{
-    EXECUCAO,     // 0
-    PRONTO,     // 1  // 5
-    CONCLUIDO      // 6
-}STATUS;
 
-// Estrutura para representar um processo
 typedef struct {
-    int id;          // Identificador do processo
-    int arrivalTime; // Tempo de chegada
-    int burstTime;   // Tempo de execução
-    int remainingTime; // Tempo restante para a conclusão
-    STATUS statusProcesso;     // Status do processo (R = em execução, P = pronto, C = concluído)
-} PROCESSO;
+    int id;
+    int Tempo_Chegada;
+    int Tempo_Execucao;
+    int Tempo_Inicio;
+    float Tempo_Execucao_Restante;
+    int Tempo_Espera;
+    int concluido,tat,rt;
+    int status;  // 0 - Pronto, 1 - Em execução, 2 - Concluído
+} Processo;
 
-// Função para encontrar o próximo processo a ser executado com base no tempo restante
-//int findShortestRemainingTime(Process processes[], int n, int currentTime) {
-//    int shortestTime = INT_MAX;
-//    int shortestIndex = -1;
-//
-//    for (int i = 0; i < n; i++) {
-//        if (processes[i].arrivalTime <= currentTime && processes[i].remainingTime < shortestTime && processes[i].remainingTime > 0 && processes[i].statusProcesso != 'C') {
-//            shortestTime = processes[i].remainingTime;
-//            shortestIndex = i;
-//        }
-//    }
-//
-//    return shortestIndex;
-//}
 
-int main() {
-    PROCESSO processos[NUM_PROCESSOS];
-    int currentTime = 0;
+int findmax(int a, int b)
+{
+    return a>b?a:b;
+}
 
-    // Inicialização dos processos
+int findmin(int a, int b)
+{
+    return a<b?a:b;
+}
+
+int main(){
+	
+	srand(time(NULL));
+	
+	Processo processos[NUM_PROCESSOS];
+	int tempo_atual = 0;
+    int processos_concluidos = 0;
+    
     for (int i = 0; i < NUM_PROCESSOS; i++) {
-        processos[i].id = i + 1;
-        printf("Informe o tempo de chegada e o tempo de execução para o processo %d: ", i + 1);
-        scanf("%d %d", &processos[i].arrivalTime, &processos[i].burstTime);
-        processos[i].remainingTime = processos[i].burstTime;
-        processos[i].statusProcesso = 'P';
-    }
-
-    printf("Tempo de Execução:\n");
-//    while (1) {
-//        int shortestIndex = findShortestRemainingTime(processos, NUM_PROCESSOS, currentTime);
+	    processos[i].Tempo_Chegada = rand() % 10;
+	    processos[i].id = i;
+	    processos[i].Tempo_Execucao = rand() % 10 + 1;
+	    processos[i].Tempo_Execucao_Restante = processos[i].Tempo_Execucao;
+	    processos[i].status = 0;
+	    printf("Processo P%d - Tempo de Chegada: %d- Tempo Execucao/Restante: %d - Status: %d\n", i,processos[i].Tempo_Chegada, processos[i].Tempo_Execucao,processos[i].status);
+	}
 //
-//        if (shortestIndex == -1) {
-//            break;
-//        }
-//
-//        processos[shortestIndex].statusProcesso = 'R';
-//        processos[shortestIndex].remainingTime--;
-//
-//        printf("Tempo %d: Processo %d\n", currentTime, processos[shortestIndex].id);
-//
-//        if (processos[shortestIndex].remainingTime == 0) {
-//            processos[shortestIndex].statusProcesso = 'C';
-//            int completionTime = currentTime + 1;
-//            printf("Processo %d concluído no tempo %d\n", processos[shortestIndex].id, completionTime);
-//        }
-//
-//        currentTime++;
+//	    for(int i=0;i<NUM_PROCESSOS;i++)
+//    {
+//        printf("\nProcesso %d Arrival Time: ",i);
+//        scanf("%d",&processos[i].Tempo_Chegada);
+//        processos[i].id = i ;
+//        processos[i].status =0;
+//    }
+//     for(int i=0;i<NUM_PROCESSOS;i++)
+//    {
+//        printf("\nEnter Process %d Burst Time: ",i);
+//       scanf("%d",&processos[i].Tempo_Execucao);
+//         processos[i].Tempo_Execucao_Restante= processos[i].Tempo_Execucao;
 //    }
 
-    return 0;
+	printf("\n");
+	
+	while(processos_concluidos!=NUM_PROCESSOS){
+		int min_index = -1;
+		int menor_tempo_execucao = INT_MAX;
+		
+		  for(int i = 0; i < NUM_PROCESSOS; i++) {
+            if(processos[i].Tempo_Chegada <= tempo_atual && processos[i].status != 2) {// verificar se aquele processo foi o primerio a chegar
+            // e se não está concluido 
+                if(processos[i].Tempo_Execucao_Restante < menor_tempo_execucao) {// verificar se o tempo faltando é o menor daqueles processos que chegaram ao mesmo tempo
+                    menor_tempo_execucao = processos[i].Tempo_Execucao_Restante;
+                    min_index = i;
+               if(processos[i].Tempo_Execucao_Restante== menor_tempo_execucao) {
+                 }
+                    if(processos[i].Tempo_Chegada < processos[min_index].Tempo_Chegada) {
+                        menor_tempo_execucao= processos[i].Tempo_Execucao_Restante;
+                        min_index = i;
+                    }
+                }
+            }
+        }
+        
+        if(min_index==-1)// não chegou nenhum processo ou todos os processos que chegaram foram completados
+        {
+            tempo_atual++;
+        }
+        else{
+        		if(processos[min_index].Tempo_Execucao_Restante == processos[min_index].Tempo_Execucao)
+            {
+                processos[min_index].Tempo_Inicio = tempo_atual;
+                processos[min_index].status = 1;
+        	}
+            processos[min_index].Tempo_Execucao_Restante -= 1;
+            tempo_atual++; 
+            if(processos[min_index].Tempo_Execucao_Restante == 0)
+            {
+                processos[min_index].concluido = tempo_atual;
+                processos[min_index].tat = processos[min_index].concluido - processos[min_index].Tempo_Chegada;
+                processos[min_index].Tempo_Espera= processos[min_index].tat - processos[min_index].Tempo_Execucao;
+                processos[min_index].rt = processos[min_index].Tempo_Inicio - processos[min_index].Tempo_Chegada;
+            
+                processos_concluidos++;
+                processos[min_index].status=2;
+            }
+        }
+		
+	}
+	
+	    //Output
+    printf("\nProcesso \t Tempo Chegada\t Tempo de Execucao CPU \tConcluido\tTAT\tEspera\tRT\n");
+    for(int i=0;i<NUM_PROCESSOS;i++)
+    printf("%d\t\t\t%d\t\t%d\t\t%d\t\t%d\t%d\t%d\n",
+	processos[i].id,processos[i].Tempo_Chegada,processos[i].Tempo_Execucao,processos[i].concluido,processos[i].tat,processos[i].Tempo_Espera,processos[i].rt);
+
+
+	return 0;
 }
